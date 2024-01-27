0c64ba54eed1d8c5c2df66d8beced37462480349
jagadeeshftw <jagadeesh26062002@gmail.com>
2024-01-27 16:30:13 +0530

    provide dynamic content with ejs

--- routes/admin.js
+++ routes/admin.js
@@ -1,20 +1,29 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const rootDir = require('../util/path');
+const rootDir = require("../util/path");
+const bodyParser = require("body-parser");
 
 const router = express.Router();
 
+
+let products = [];
+let pageName = "Add Product";
+
+router.use(bodyParser.urlencoded({ extended: false }));
+
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+router.get("/add-product", (req, res, next) => {
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 // /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
+
+
+router.post("/add-product", (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect("/");
 });
 
-module.exports = router;
+module.exports = { router, products };
--- routes/shop.js
+++ routes/shop.js
@@ -1,13 +1,21 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const app = require("express");
 
-const rootDir = require('../util/path');
+const rootDir = require("../util/path");
+const bodyParser = require("body-parser");
 
-const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+const adminRoutes = require("./admin")
+
+const router = app.Router();
+
+router.use(bodyParser.urlencoded({ extended: false }));
+
+let pageName = "Shop";
+let prod = adminRoutes.products;
+router.get("/", (req, res, next) => {
+  res.render("shop", { prod, pageName });
 });
 
 module.exports = router;
